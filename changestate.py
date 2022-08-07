dfc2dbfbd7a436caf1dd01563aeab8ef39d7079c
Danilo Santos <danilosantos1986@gmail.com>
2022-08-06 21:58:19 -0300

    Start vm if mac address math with virtual machine

--- changestate.py
+++ changestate.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 
 import mac
+from os import system
 from subprocess import Popen, PIPE, STDOUT
 
 from wakeonlan import send_magic_packet
@@ -11,6 +12,10 @@ def wake(host):
 	mac_list = mac.get(host)
 	mac_address = ''
 	for mac_address in mac_list:
+		if mac_address[:8] == b'08:00:27':
+			print(f'Iniciando VM {host}')
+			system(f'vboxmanage startvm {host} --type=headless')
+			break
 		send_magic_packet(mac_address.decode(), interface="10.0.0.0")
 
 def shutdown(host):
