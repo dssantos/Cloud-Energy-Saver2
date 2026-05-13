5b0365749101bed1e7f88212b861b1cf8d4b4936
Danilo Santos <danilosantos1986@gmail.com>
2026-05-12 22:41:21 -0300

    Add remote VirtualBox VM control via SSH

--- changestate.py
+++ changestate.py
@@ -1,11 +1,19 @@
 #coding: utf-8
 
 import mac
+import os
+from dotenv import load_dotenv
 from os import system
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE, STDOUT, CalledProcessError
 
 from wakeonlan import send_magic_packet
 
+# Carregar variáveis de ambiente
+load_dotenv()
+
+# Configuração do Windows com VirtualBox
+WINDOWS_HOST = os.getenv('WINDOWS_HOST')
+WINDOWS_USER = os.getenv('WINDOWS_USER')
 
 def wake(host):
 
@@ -14,9 +22,23 @@ def wake(host):
 	for mac_address in mac_list:
 		if mac_address[:8] == b'08:00:27':
 			print(f'Iniciando VM {host}')
-			system(f'vboxmanage startvm {host} --type=headless')
+			# Tentar VBoxManage local primeiro
+			try:
+				result = system(f'vboxmanage startvm {host} --type=headless')
+				if result == 0:
+					break
+				print('VBoxManage local falhou, tentando via SSH...')
+			except:
+				print('VBoxManage local não disponível, tentando via SSH...')
+
+			# Fallback: Executar VBoxManage remotamente no Windows via SSH
+			vbox_cmd = f'ssh {WINDOWS_USER}@{WINDOWS_HOST} "VBoxManage startvm {host} --type=headless"'
+			system(vbox_cmd)
 			break
-		send_magic_packet(mac_address.decode(), interface="10.0.0.0")
+		else:
+			if mac_address != b'00:00:00:00:00:00':
+				print(f'Enviando Wake-on-LAN para {host}')
+				send_magic_packet(mac_address.decode(), interface="10.0.0.0")
 
 def shutdown(host):
 
